13df5ef007cd6e211b41248309ed79494eb60421
MickeyDLuffy <thefalconslayer@gmail.com>
2023-07-08 15:12:39 +0000

    VACAY-12 - create new repo and bootstrap vacations api

---
+++ src/main/java/com/github/mickeydluffy/VacationsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.mickeydluffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationsApiApplication.class, args);
+    }
+
+}
