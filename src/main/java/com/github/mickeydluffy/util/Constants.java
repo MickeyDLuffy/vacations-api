85c75cec65f08e8015c2ccc4d1f5ac52ab543955
MickeyDLuffy <thefalconslayer@gmail.com>
2023-07-11 00:15:50 +0000

    VACAY-17 - add controller advice and constants final class

---
+++ src/main/java/com/github/mickeydluffy/util/Constants.java
@@ -0,0 +1,9 @@
+package com.github.mickeydluffy.util;
+
+public final class Constants {
+    public static final String API_VERSION = "/api/v1";
+
+    private Constants() {
+        throw new IllegalStateException("Cannot instantiate utility class");
+    }
+}
